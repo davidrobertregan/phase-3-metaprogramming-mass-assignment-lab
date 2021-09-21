c61401e5e278a0c2b670f8dba951e4638b855dcb
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-09-21 15:51:42 -0500

    commit tree 57fc2491f0d8c995851894de4a8bbdf48a614c25

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 class Person
-  # your code here
+  
+  def initialize(attr)
+    attr.each do |key, value| 
+      self.class.attr_accessor key
+      self.send "#{key}=", value
+    end
+  end
+
 end
+
+bob_attributes = { name: "Bob", hair_color: "Brown" }
+
+bob = Person.new bob_attributes
+
+binding.pry
